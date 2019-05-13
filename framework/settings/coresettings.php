3541110d1dc595e34195da283fc5752b0decdec7
Dory Azar <dory.azar@gmail.com>
2019-05-13 14:15:40 -0400

    Adding standalone

--- framework/settings/coresettings.php
+++ framework/settings/coresettings.php
@@ -21,7 +21,7 @@ session('scripts', array(  'favicon' => $_SESSION['imagesUrl'].getenv('FAVICON')
                    'fallback_html5shiv' => 'https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js',
                    'fallback_respond' => 'https://oss.maxcdn.com/respond/1.4.2/respond.min.js',
                    'lightbox_script' => "<script src='../js/lightbox/lightbox.js'></script><script>lightbox.option({'alwaysShowNavOnTouchDevices': true,'disableScrolling': true})</script>",
-                   'download_script' => "<script src='../js/addtohome/src/addtohomescreen.js'></script><script>var ath = addToHomescreen({skipFirstVisit: false, startDelay: 0, lifespan: 0, displayPace: 0, maxDisplayCount: 0 });</script>",
+                   'add_to_home' => $_SESSION['public'].'js/addtohome/src/addtohomescreen.js',
                    'wysiwyg_css' => 'https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.css',
                    'wysiwyg_js' => 'https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js',
                    'social_buttons_css' => $_SESSION['public'].'css/social_buttons.css'
@@ -34,10 +34,6 @@ session('icons', "<link rel='apple-touch-icon' sizes='57x57' href='' />
 <link rel='apple-touch-icon' sizes='114x114' href='' />
 <link rel='apple-touch-icon' sizes='144x144' href='' />");
 
-// Initialize phone standalone script
-session('standalone', array('ios' => "<meta name='apple-mobile-web-app-capable' content='yes'>
-                               <meta name='apple-mobile-web-app-status-bar-style' content='black' />",
-                     'android' => "<meta name='mobile-web-app-capable' content='yes'>"));
 
 // Initialize metadata
 session('metadata', ['site_name' => '', 'site_url' => '', 'creator' => '','title' => '', 'image' => '', 'description' => '', 'amount' => '', 'currency' => '', 'shipping_cost' => '', 'shipping_cost_currency' => ''] );
