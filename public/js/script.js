87ced4f227fa529895cc8f9bcee9d0201a979de3
Dory Azar <dory.azar@gmail.com>
2019-08-19 18:41:15 -0400

    Reverting

--- public/js/script.js
+++ public/js/script.js
@@ -103,20 +103,6 @@ function getIdOnScreen(element) {
     return top;
 }
 
-function animateCSS(element, animationName, callback) {
-    const node = document.querySelector(element)
-    node.classList.add('animated', animationName)
-
-    function handleAnimationEnd() {
-        node.classList.remove('animated', animationName)
-        node.removeEventListener('animationend', handleAnimationEnd)
-
-        if (typeof callback === 'function') callback()
-    }
-
-    node.addEventListener('animationend', handleAnimationEnd)
-}
-
 /* Social buttons*/
 $(function () {
   var all_classes = "";
