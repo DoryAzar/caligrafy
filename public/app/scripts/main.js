162c70181eb0b13ebefd7abf304a58dca8ee62b1
Dory Azar <dory.azar@gmail.com>
2019-12-02 14:08:18 -0500

    Changing the default app mainjs

--- public/app/scripts/main.js
+++ public/app/scripts/main.js
@@ -3,34 +3,33 @@ var app = new Vue({
   data () {
     return {
         response: null,
-        env: env
+        env: env,
+        config: {
+            async: true,
+            crossDomain: true,
+            headers: {
+                "Authorization": "Bearer " + apiKey,
+                'Content-Type': 'application/json',
+                 'Set-Cookie': 'widget_session=caligrafy_app; SameSite=None; Secure'
+                }
+        }
 
     }
   },
   /* Method Definition  */
   methods: {
       
-      // example of axios get method with default parameters
+      // API get call using axios
       all: function(route) {
           axios.get(route)
               .then(response => (this.response = response.data))
               .catch(error => (console.log(error)));
-      },
-      // example of axios method with additional parameters such as headers
-      allWithHeaders: function(route) {
-            axios.get(route, {async: true, crossDomain: true, headers: {
-                  'Access-Control-Allow-Origin': '*', 
-                  'Accept': 'application/json', 
-                  'Content-Type': 'application/json',
-                  'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
-                  'Access-Control-Allow-Headers': '*'}}
-                  )
-              .then(response => (this.response = response.data))
-      },
+      }
+      
   },
   /* upon object load, the following will be executed */
   mounted () {
-      this.allWithHeaders('/caligrafy');
+      this.all(env.home);
   }
 
 });
